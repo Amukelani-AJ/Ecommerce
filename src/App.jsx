ebbdc7a3ec19dda784d4ad244956c2dc8b4926cd
Amukelani-AJ <ndlovuamukelanij@gmail.com>
2026-02-27 14:51:16 +0200

    Added CheckoutPage

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,14 @@
 import { HomePage } from './pages/HomePage'
 import { Routes, Route } from 'react-router';
+import { CheckoutPage } from './pages/CheckoutPage';
 import './App.css'
 
 function App() {
  
   return (
     <Routes>
-      <Route path="/" element={ <HomePage/>}/>
-      <Route path="checkout"/>
+      <Route index element={ <HomePage/>}/>
+      <Route path="checkout" element={<CheckoutPage/>}/>
     </Routes>
    
   );
